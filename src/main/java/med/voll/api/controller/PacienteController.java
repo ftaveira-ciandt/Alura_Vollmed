606d825409b11cb005586e09f12bb4119002fe85
Francisco Taveira <ftaveira@ciandt.com>
2023-02-22 15:38:32 -0300

    Patient class created for repository implementation

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,8 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class PacienteController {
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dadospaciente) {
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dadospaciente) {
+        repository.save(new Paciente(dadospaciente));
         System.out.println("dados recebidos" + dadospaciente);
     }
 
