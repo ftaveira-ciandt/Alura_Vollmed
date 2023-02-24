896f588d25476a4e654189ecd5a7dd0c976a038d
Francisco Taveira <ftaveira@ciandt.com>
2023-02-24 09:19:11 -0300

    Create class DadosListagemPaciente

--- src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -1,5 +1,9 @@
 package med.voll.api.paciente;
 
-public record DadosListagemPaciente() {
+public record DadosListagemPaciente(String nome, String email, String cpf, String telefone) {
+
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf());
+    }
 
 }
