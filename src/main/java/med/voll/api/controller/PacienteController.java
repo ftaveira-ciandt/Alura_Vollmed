b7f31f9b2dc9986f6546af2eb7de71a30a8708e7
Francisco Taveira <ftaveira@ciandt.com>
2023-02-24 09:15:44 -0300

    created register and list method in the controller class

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,21 +2,38 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
 
+
+    @Autowired
+    private PacienteRepository repository;
+
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dadospaciente) {
-        repository.save(new Paciente(dadospaciente));
-        System.out.println("dados recebidos" + dadospaciente);
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
+
+
+    }
+
+    @GetMapping
+    public List<DadosListagemPaciente> listar() {
+        return repository.findAll().stream().map(DadosListagemPaciente::new).toList();
     }
 
 }
