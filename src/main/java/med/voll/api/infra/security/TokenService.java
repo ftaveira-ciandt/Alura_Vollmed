26417f0379678802ef95ccc1df7fb804cb927f8f
Francisco Taveira <ftaveira@ciandt.com>
2023-05-22 14:50:49 -0300

    authenticathor jwt ok

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -13,10 +14,14 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     public String gerarToken(Usuario usuario) {
+
         try {
-           var algoritmo  = Algorithm.HMAC256("12345678");
-           return JWT.create().withIssuer("API VollMed").withSubject(usuario.getLogin()).withExpiresAt(dataExpiracao()).sign(algoritmo);
+           var algoritmo  = Algorithm.HMAC256(secret);
+           return JWT.create().withIssuer("API VollMed").withSubject(usuario.getLogin()).withClaim("id", usuario.getId()).withExpiresAt(dataExpiracao()).sign(algoritmo);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token jwt", exception);
         }
